6e74ea1cf93c5d75cc0b067abfc5654e85fe4438
cman-oss <website1232@outlook.com>
2025-03-08 19:05:37 +0000

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/chemical-synthesis-app/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
